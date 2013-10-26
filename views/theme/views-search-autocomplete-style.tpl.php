a4189b29c1ad58c537bdf4a2eb2059df285c520a
Miroslav <miroslav@axiomcafe.fr>
2013-10-26 16:18:53 +0200

    Commit for coding rewrite and correction purpose to validate as: http://pareview.sh/

--- views/theme/views-search-autocomplete-style.tpl.php
+++ views/theme/views-search-autocomplete-style.tpl.php
@@ -1,30 +1,31 @@
 <?php
 /**
- * @file views-views-json-style.tpl.php
+ * @file 
+ * views-views-json-style.tpl.php
  *
  * - $view: The View object.
- * - $rows: Array of row objects as rendered by _search_autocomplete_render_fields
- *   $options: Array of plugin style options
+ * - $rows: Array of row objects as rendered by 
+ * _search_autocomplete_render_fields
+ * - $options: Array of plugin style options
  *
  * @ingroup views_templates
  * @see views_plugin_style_json.inc
  * @see views_search_autocomplete_style.theme.inc
  */
 
-if ($view->override_path) {
+if ($view->override_path) :
   // We're inside a live preview where the JSON is pretty-printed.
   $json = _search_autocomplete_encode_formatted($rows);
   print "<code>$json</code>";
-}
-else {
+else :
   $json = _search_autocomplete_json_encode($rows, $bitmask);
 
-  // We want to send the JSON as a server response so switch the content
-  // type and stop further processing of the page.
+  // We want to send the JSON as a server response so switch the content.
+  // Type and stop further processing of the page.
   $content_type = 'application/json';
   drupal_add_http_header("Content-Type", "$content_type; charset=utf-8");
   print $json;
-  //Don't think this is needed in .tpl.php files: module_invoke_all('exit');
+  // Don't think this is needed in .tpl.php files: module_invoke_all('exit');
   exit;
 
 }
