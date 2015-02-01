8a207a533522195f1318ebf3afc4d9975f5e4882
Dom <contact@drupal-addict.com>
2015-02-01 18:17:41 +0100

    [DCE] Commit basic CRUD methods and links for new Autocompletion Configuration.

---
+++ src/Controller/AutocompletionConfigurationListBuilder.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\search_autocomplete\Controller\AutocompletionConfigurationListBuilder.
+ */
+
+namespace Drupal\search_autocomplete\Controller;
+
+use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Provides a listing of autocompletion_configuration entities.
+ *
+ * List Controllers provide a list of entities in a tabular form. The base
+ * class provides most of the rendering logic for us. The key functions
+ * we need to override are buildHeader() and buildRow(). These control what
+ * columns are displayed in the table, and how each row is displayed
+ * respectively.
+ *
+ * Drupal locates the list controller by looking for the "list" entry under
+ * "controllers" in our entity type's annotation. We define the path on which
+ * the list may be accessed in our module's *.routing.yml file. The key entry
+ * to look for is "_entity_list". In *.routing.yml, "_entity_list" specifies
+ * an entity type ID. When a user navigates to the URL for that router item,
+ * Drupal loads the annotation for that entity type. It looks for the "list"
+ * entry under "controllers" for the class to load.
+ *
+ * @package Drupal\search_autocomplete\Controller
+ *
+ * @ingroup search_autocomplete
+ */
+class AutocompletionConfigurationListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * Builds the header row for the entity listing.
+   *
+   * @return array
+   *   A render array structure of header strings.
+   *
+   * @see Drupal\Core\Entity\EntityListController::render()
+   */
+  public function buildHeader() {
+    $header['enable'] = $this->t('Activation');
+    $header['label'] = $this->t('Autocompletion Configuration');
+    $header['machine_name'] = $this->t('Machine Name');
+    $header['floopy'] = $this->t('Floopy');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * Builds a row for an entity in the entity listing.
+   *
+   * @param EntityInterface $entity
+   *   The entity for which to build the row.
+   *
+   * @return array
+   *   A render array of the table row for displaying the entity.
+   *
+   * @see Drupal\Core\Entity\EntityListController::render()
+   */
+  public function buildRow(EntityInterface $entity) {
+    $row['enable'] = array(
+        '#type' => 'checkbox',
+        '#title' => $this->t('Enable'),
+        '#default_value' => true,
+        '#disabled' => false,
+    );
+    $row['label'] = $this->getLabel($entity);
+    $row['machine_name'] = $entity->id();
+    $row['floopy'] = $entity->floopy;
+
+    return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * Adds some descriptive text to our entity list.
+   *
+   * Typically, there's no need to override render(). You may wish to do so,
+   * however, if you want to add markup before or after the table.
+   *
+   * @return array
+   *   Renderable array.
+   */
+//   public function render() {
+//     $build['description'] = array(
+//       '#markup' => $this->t("<p>The Config Entity Example module defines a"
+//         . " AutocompletionConfiguration entity type. This is a list of the AutocompletionConfiguration entities currently"
+//         . " in your Drupal site.</p><p>By default, when you enable this"
+//         . " module, one entity is created from configuration. This is why we"
+//         . " call them Config Entities. Marvin, the paranoid android, is created"
+//         . " in the database when the module is enabled.</p><p>You can view a"
+//         . " list of AutocompletionConfigurations here. You can also use the 'Operations' column to"
+//         . " edit and delete AutocompletionConfigurations.</p>"),
+//     );
+//     $build[] = parent::render();
+//     return $build;
+//   }
+
+}
---
+++ src/Entity/AutocompletionConfiguration.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\search_autocomplete\Entity\AutocompletionConfiguration.
+ *
+ * This contains our Autocompletion Configuration config entity class. This
+ * entity represents a configuration to enable autocompletion on a field.
+ */
+
+namespace Drupal\search_autocomplete\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+
+/**
+ * Defines the autocompletion_configuration entity.
+ *
+ * The lines below, starting with '@ConfigEntityType,' are a plugin annotation.
+ * These define the entity type to the entity type manager.
+ *
+ * The properties in the annotation are as follows:
+ *  - id: The machine name of the entity type.
+ *  - label: The human-readable label of the entity type. We pass this through
+ *    the "@Translation" wrapper so that the multilingual system may
+ *    translate it in the user interface.
+ *  - controllers: An array specifying controller classes that handle various
+ *    aspects of the entity type's functionality. Below, we've specified
+ *    controllers which can list, add, edit, and delete our autocompletion_configuration entity, and
+ *    which control user access to these capabilities.
+ *  - config_prefix: This tells the config system the prefix to use for
+ *    filenames when storing entities. Because we don't specify, it will be
+ *    the module's name. This means that the default entity we include in our
+ *    module has the filename
+ *    'search_autocomplete.autocompletion_configuration.xxx.yml'.
+ *  - entity_keys: Specifies the class properties in which unique keys are
+ *    stored for this entity type. Unique keys are properties which you know
+ *    will be unique, and which the entity manager can use as unique in database
+ *    queries.
+ *
+ * @see annotation
+ * @see Drupal\Core\Annotation\Translation
+ *
+ * @ingroup search_autocomplete
+ *
+ * @ConfigEntityType(
+ *   id = "autocompletion_configuration",
+ *   label = @Translation("autocompletion configuration"),
+ *   admin_permission = "administer search autocomplete",
+ *   handlers = {
+ *     "access" = "Drupal\search_autocomplete\AutocompletionConfigurationAccessControlHandler",
+ *     "list_builder" = "Drupal\search_autocomplete\Controller\AutocompletionConfigurationListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\search_autocomplete\Form\AutocompletionConfigurationAddForm",
+ *       "edit" = "Drupal\search_autocomplete\Form\AutocompletionConfigurationEditForm",
+ *       "delete" = "Drupal\search_autocomplete\Form\AutocompletionConfigurationDeleteForm"
+ *     }
+ *   },
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label"
+ *   },
+ *   links = {
+ *     "edit-form" = "/examples/search_autocomplete/manage/{autocompletion_configuration}",
+ *     "delete-form" = "/examples/search_autocomplete/manage/{autocompletion_configuration}/delete"
+ *   }
+ * )
+ */
+class AutocompletionConfiguration extends ConfigEntityBase {
+
+  /**
+   * The autocompletion_configuration ID.
+   *
+   * @var string
+   */
+  public $id;
+
+  /**
+   * The autocompletion_configuration UUID.
+   *
+   * @var string
+   */
+  public $uuid;
+
+  /**
+   * The autocompletion_configuration label.
+   *
+   * @var string
+   */
+  public $label;
+
+  /**
+   * The autocompletion_configuration floopy flag.
+   *
+   * @var string
+   */
+  public $floopy;
+}
