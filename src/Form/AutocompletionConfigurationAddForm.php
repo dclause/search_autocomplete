7232f3e33acef169dc8e9a5f830d794ea743037d
Dom <contact@drupal-addict.com>
2015-02-06 17:35:03 +0100

    [DCE] Commit default configuration and basic unit testings.

--- src/Form/AutocompletionConfigurationAddForm.php
+++ src/Form/AutocompletionConfigurationAddForm.php
@@ -39,4 +39,40 @@ class AutocompletionConfigurationAddForm extends AutocompletionConfigurationForm
     return $actions;
   }
 
+
+  /**
+   * Overrides Drupal\Core\Entity\EntityFormController::form() and
+   * Drupal\search_autocomplete\Form\AutocompletionConfigurationFormBase::form()
+   *
+   * Builds the entity add form.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param array $form_state
+   *   An associative array containing the current state of the form.
+   *
+   * @return array
+   *   An associative array containing the autocompletion_configuration
+   *   add/edit form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    // Get anything we need from the base class.
+    $form = parent::buildForm($form, $form_state);
+
+    // Build the form.
+
+    // Selector
+    $form['selector'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('ID selector this configuration should apply to'),
+      '#description' => 'Enter a valid query selector for this configuration. This should be an ID or a class targeting an input field.',
+      '#default_value' => $this->entity->getSelector(),
+      '#required' => TRUE,
+    );
+
+    // Return the form.
+    return $form;
+  }
+
 }
